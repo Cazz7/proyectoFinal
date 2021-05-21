cab2d8b0dcddba7dd374ff4b007c08e2fa0c16bf
cazz7 <cazz182@gmail.com>
2021-05-21 03:29:59 +0000

    Proyecto final terminado. Pendiente revision y pruebas

--- Employees/webapp/controller/Main.controller.js
+++ Employees/webapp/controller/Main.controller.js
@@ -8,17 +8,29 @@ sap.ui.define([
 
         onCreateEmployee: function () {
             let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteCreateEmployee", { });
+            oRouter.navTo("RouteCreateEmployee", {});
         },
 
         onListEmployee: function () {
             let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteListEmployee", { });
+            oRouter.navTo("RouteListEmployee", {});
         },
 
         onSignOrder: function () {
             let oRouter = sap.ui.core.UIComponent.getRouterFor(this);
-            oRouter.navTo("RouteSignOrder", { });
+            oRouter.navTo("RouteSignOrder", {});
         },
+
+        onAfterRendering: function () {
+            // Error en el framework: Al agregar la dirección URL de "Firmar
+            //pedidos", el componente GenericTile debería navegar directamente a dicha URL,
+            // pero no funciona en la versión 1.78. Por tanto, una solución encontrada es
+            //eliminando la propiedad id del componente por jquery
+            var genericTileFirmarPedido = this.byId("firmarPedidoTile");
+            //Id del dom
+            var idGenericTileFirmarPedido = genericTileFirmarPedido.getId();
+            //Se vacía el id
+            jQuery("#" + idGenericTileFirmarPedido)[0].id = "";
+        }
     });
 });
